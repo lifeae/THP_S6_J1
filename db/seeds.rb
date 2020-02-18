4a04da7ace57f9c004ec9dd17d8f66e2b5ff2c54
lifeae <romain.leporatti@laposte.net>
2020-02-18 21:30:09 +0100

    db, seeds.rb done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Destruction des tables :"
+User.destroy_all
+print "User, "
+Event.destroy_all
+print "Event, "
+Attendance.destroy_all
+print "Attendance."
+
+
+puts "\n\nCréation des users"
+u = 1
+users = 10.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email(domain: 'yopmail.com'),
+    description: Faker::Lorem.sentences,
+    encrypted_password: Faker::Device.serial)
+    print "User #{u} crée ! "
+    u += 1
+end
+
+puts "\n\nCréation des events"
+e = 1
+events = 10.times do
+  Event.create!(
+    start_date: Faker::Date.forward(days: 365),
+    duration: Faker::Number.between(from: 1, to: 240),
+    title: Faker::Lorem.sentence(word_count: 3),
+    description: Faker::Lorem.sentences,
+    price:Faker::Commerce.price,
+    location:Faker::Address.full_address)
+    print "Event #{e} crée ! "
+    e += 1
+end
+
+puts "\n\nCréation des attendances"
+a = 1
+10.times do
+  Attendance.create!(stripe_customer_id: Faker::Finance.credit_card,
+    event: Event.all.sample,
+    user: User.all.sample)
+  print "Attendance #{a} crée ! "
+  a += 1
+end
+
+puts "\n\nCréation des seeds terminée !"
