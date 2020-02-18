c1eec0b9167959283a6b4e617e41e2f991df07ba
lifeae <romain.leporatti@laposte.net>
2020-02-18 21:36:55 +0100

    null: false, default: '' add in user migration on :email and :encrypted_password

--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 2020_02_18_181309) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "encrypted_password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.text "description"
     t.string "first_name"
     t.string "last_name"
